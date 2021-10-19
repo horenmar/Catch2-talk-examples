50d834e3752ea08ea2b058e1311dd387968e8852
Martin Hořeňovský <martin.horenovsky@gmail.com>
2021-10-19 13:28:35 +0200

    Add section showcases

--- simple-usage.cpp
+++ simple-usage.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <iostream>
+
 int factorial(int i) {
     if (i < 2) {
         return i;
@@ -12,4 +14,30 @@ TEST_CASE("Basic example") {
     REQUIRE(factorial(0) == 1);
     REQUIRE(factorial(1) == 1);
     REQUIRE(factorial(2) == 2);
+}
+
+TEST_CASE("Section showcase 1") {
+    std::cout << '1';
+    SECTION("A") {
+        std::cout << 'A';
+    }
+    SECTION("B") {
+        std::cout << 'B';
+    }
+    std::cout << '\n';
+}
+
+TEST_CASE("Section showcase 2") {
+    std::cout << '1';
+    SECTION("A") {
+        std::cout << 'A';
+        SECTION("a") { std::cout << 'a'; }
+        SECTION("b") { std::cout << 'b'; }
+    }
+    SECTION("B") {
+        std::cout << 'B';
+        SECTION("a") { std::cout << 'a'; }
+        SECTION("b") { std::cout << 'b'; }
+    }
+    std::cout << '\n';
 }
